af827de86f36ed530ae1cee389214cdef1db3b7e
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-01-07 15:36:51 -0500

    improved the AsyncStreams example

---
+++ Samples/Samples/AsyncStreams/Consumer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.AsyncStreams
+{
+    class Consumer
+    {
+        public async Task Consume()
+        {
+            await foreach (var name in Producer.GetAllNames())
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Samples/Samples/AsyncStreams/Producer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Samples.AsyncStreams
+{
+    public static class Producer
+    {
+        public static async IAsyncEnumerable<string> GetAllNames()
+        {
+            var pageIndex = 0;
+            const int pageSize = 100;
+            var hasMore = false;
+            do
+            {
+                // ****IMPORTANT ****
+                // Never EVER do this in a real application. The following block of code is
+                // not secure and could be subject to an attack known as SQL Injection.
+                await using var conn = new SqlConnection("ConnectionString here");
+                await using var cmd = new SqlCommand(
+                    @$"
+                    SELECT Name 
+                    FROM Users
+                    ORDER BY Name
+                    OFFSET {pageIndex * pageSize} ROWS
+                    FETCH NEXT {pageSize} ROWS ONLY",
+                    conn);
+                await using var reader = await cmd.ExecuteReaderAsync();
+                while (reader.Read())
+                {
+                    yield return reader.GetString(0); // This is the name column
+                }
+
+                pageIndex++;
+                hasMore = reader.HasRows;
+            } while (hasMore);
+        }
+    }
+}
